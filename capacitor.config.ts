38db246bcc29ecd49a42f940ff726e738dedfd26
LAPTOP-RI8UVELL\rafab <rafa.bope.96@gmail.com>
2024-08-02 23:34:28 +0100

    Inicial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'reservation.management',
+  appName: 'reservation-management',
+  webDir: 'www'
+};
+
+export default config;
