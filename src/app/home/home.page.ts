1d672c1c7c4fc979b52e0f14d4ec4dbf385c0ad4
LAPTOP-RI8UVELL\rafab <rafa.bope.96@gmail.com>
2024-08-11 11:59:34 +0100

    Mockup de menu principal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { AddProdutoComponent } from '../components/add-produto/add-produto.component';
+import { MainMenuComponent } from '../components/main-menu/main-menu.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [AddProdutoComponent,IonicModule],
+  imports: [AddProdutoComponent,IonicModule,MainMenuComponent],
 })
 export class HomePage {
   constructor() {}
